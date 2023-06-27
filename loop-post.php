6893fd1a7030ffc43b5fc3357e65ff76991c3d84
denisijka <37539630+denisijka@users.noreply.github.com>
2023-06-28 00:01:03 +0300

    Update loop-post.php

--- loop-post.php
+++ loop-post.php
@@ -1,35 +1,47 @@
-<?php if ( have_rows( 'constructor' ) ) :
-    while ( have_rows( 'constructor' ) ) : the_row(); ?>
-
 <div class="wrap-post wrap-post-<?php echo $args['i']; ?>" >
     <div class="row">
         <div class="col-12">
-            <div class="title style-1"><?php the_title(); ?></div>
-        </div>
-        <div class="col-4">
-            <div class="wrap-img ">
-                <?php
-                $image = get_field('image');
-                if( !empty( $image ) ): ?>
-                    <img class="h100" src="<?php echo esc_url($image['url']); ?>" alt="<?php echo esc_attr($image['alt']); ?>" />
-                <?php endif; ?>
+            <div class="wrap-title text-center">
+                <div class="js-box-speak d-inline-block title style-1 decore-icon decore-<?php the_field('decore'); ?>">
+                    <?php the_title(); ?>
+                </div>
             </div>
         </div>
-        <div class="col-8 ">
-            <div class="wrap-text d-flex h100 text style-1">
-
-                <div class="col-6 offset-lg-1"><?php echo get_sub_field( 'editor_1' ); ?></div>
-                <div class="col-6 offset-lg-1"><?php echo get_sub_field( 'editor_2' ); ?></div>
-                
-                <?php
-                // ! підбери кількість символів що виводяться
-                echo wp_trim_words(get_the_content(), 40, '...'); ?>
-            </div>
-            <div class="wrap-btn">
-                <a class="btn style-1 w100" href="<?php the_permalink(); ?>">переглянути більше</a>
+        <?php if ( is_single() ) : ?>
+        <div class="col-12">
+            <div class="wrap-btn-speak text-center">
+                <span class="btn style-2 js-btn-speak">
+                    <span>
+                        <i>Прочитати статтю...</i>
+                        <i>Зупитити читання...</i>
+                    </span>
+                </span>
             </div>
         </div>
-
+        <?php endif; ?>
+        <div class="col-sm-4 col-12">
+            <div class="box-img h100">
+                <div class="wrap-img h100" style="background-image: url(<?php echo get_field('image')['url']; ?>)"></div>
+            </div>
+        </div>
+        <div class="col-sm-8 col-12">
+            <div class="wrap-text h100 text style-1">
+                <div class="row">
+                    <div class="js-box-speak col-12 col-sm-5 offset-sm-1 "><?php echo get_field( 'editor_1' ); ?></div>
+                    <div class="js-box-speak <?php if ( !is_single() ) : ?>d-none d-sm-block <?php endif; ?> col-12 col-sm-5 offset-sm-1"><?php echo get_field( 'editor_2' ); ?></div>
+                </div>
+            </div>
+        </div>
+        <?php if ( !is_single() ) : ?>
+        <div class="col-12">
+            <div class="row">
+                <div class="col-12 col-sm-7 offset-sm-5 d-flex justify-content-center">
+                    <div class="wrap-btn w100">
+                        <a class="btn style-1 w100" href="<?php the_permalink(); ?>">переглянути більше</a>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <?php endif; ?>
     </div>
 </div>
-<?php endwhile; endif; ?>
